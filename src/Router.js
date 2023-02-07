d6f8946dcd2c91b5b6a4cb6aecc0fc88a9fd97f9
shinheylynn <heylub@gmail.com>
2023-02-07 21:49:22 +0900

    add: feature/signup layout in progress

--- src/Router.js
+++ src/Router.js
@@ -13,7 +13,7 @@ import Footer from './components/Footer/Footer';
 const Router = () => {
   return (
     <BrowserRouter>
-      <Nav />
+      {/* <Nav /> */}
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/signup" element={<SignUp />} />
@@ -23,7 +23,7 @@ const Router = () => {
         <Route path="/cart" element={<Cart />} />
         <Route path="/payment" element={<Payment />} />
       </Routes>
-      <Footer />
+      {/* <Footer /> */}
     </BrowserRouter>
   );
 };
--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -2,7 +2,43 @@ import React from 'react';
 import './SignUp.scss';
 
 const SignUp = () => {
-  return <p>안녕하세요</p>;
+  return (
+    <div className="signupContainer">
+      <div className="membershipBox">
+        <h6 className="membership">나이키 멤버십</h6>
+      </div>
+      <div className="titleBox">
+        <h1 className="title">우리 모두 선수로서 하나되는 곳</h1>
+        <p className="titlePara">
+          나이키와 함께 새로운 미래를 만들어 보세요.
+          <br />
+          나이키 멤버가 되어 최고의 제품을 가장 먼저 만나보고,
+          <br />
+          새롭게 도전하는 다양한 커뮤니티와 함께 일상 속 영감을 받아보세요.
+        </p>
+        <div className="joinMembershipBtnBox">
+          <button className="joinMembershipBtn">멤버 가입하기</button>
+        </div>
+      </div>
+      <div className="videoBox">
+        <div className="video" />
+      </div>
+      <div className="memBenefitBox">
+        <div className="memBenefit">나이키 멤버 혜택</div>
+        <section className="welcomeCoupon">
+          <div className="welcomeImage" /> {/* TODO: img로 변경 */}
+          <div className="welcomeInfo">
+            <h2 className="welcomeTitle">웰컴 쿠폰</h2>
+            <p className="welcomePara">
+              나이키 신규 멤버에게 문자로 발급되는 웰컴 쿠폰(1만원 할인코드)을
+              통해 <br /> 첫 구매부터 할인 혜택까지 만나보세요.
+            </p>
+            <button className="welcomeDetailBtn">자세히 보기</button>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
