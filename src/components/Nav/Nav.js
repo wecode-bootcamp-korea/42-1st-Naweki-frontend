cb291a68dc0f6d4892cbce3337daffb54771e935
macbook <macbook@jungee.local>
2023-02-07 20:36:48 +0900

    Add: nav 레이아웃

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,8 +1,31 @@
 import React from 'react';
+// import { menuData } from './data/menuData';
 import './Nav.scss';
 
 const Nav = () => {
-  return <p>안녕하세요</p>;
+  return (
+    <nav className="nav">
+      <div className="logoWrapper">
+        <img className="logo" alt="Logo Img" src="images/logo.jpg" />
+      </div>
+      <div className="menuWrapper">
+        <ul className="menuLists">
+          <li className="menuList">New Releases</li>
+          <li className="menuList">Men</li>
+          <li className="menuList">Women</li>
+          <li className="menuList">Kids</li>
+          <li className="menuList">Sale</li>
+          <li className="menuList">SNEAKERS</li>
+          <li className="menuList">FEEL YOUR ALL</li>
+        </ul>
+      </div>
+      <div className="functionWrapper">
+        <input className="searchBar" type="text" placeholder="검색" />
+        <i className="fa-light fa-heart" />
+        <i className="fa-light fa-cart-shopping" />
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
