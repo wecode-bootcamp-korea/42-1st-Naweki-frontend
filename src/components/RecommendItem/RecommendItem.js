12a6b31bd59adbdf21bdb0a2461771c748a3d16e
이유진 <yjoct13@gmail.com>
2023-02-10 01:37:06 +0900

    추천 상품 슬라이드 구현중

--- src/components/RecommendItem/RecommendItem.js
+++ src/components/RecommendItem/RecommendItem.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './RecommendItem.scss';
 
 function RecommendItem({ shoesImg, shoesName, category, price }) {
+  // const [recommendItem, setRecommendItem] = useState([]);
+
+  // useEffect(() => {
+  //   fetch('/data/shoesData.json')
+  //     .then(res => res.json())
+  //     .then(data => setRecommendItem(data));
+  // }, []);
+  // const [itemBox, setItemBox] = useState([]);
   return (
     <div className="recommendItem">
-      <img className="shoesImg" src={shoesImg} alt="{shoesName}" />
-      <div className="shoesInfo">
-        <div className="shoesName">{shoesName}</div>
-        <div className="price">{price} 원</div>
+      {/* <div className="arrowBtn">
+        <img
+          className="leftBtn"
+          src="https://cdn-icons-png.flaticon.com/128/2722/2722991.png"
+          alt="왼쪽 화살표"
+        />
+        <img
+          className="rightBtn"
+          src="https://cdn-icons-png.flaticon.com/128/2722/2722985.png"
+          alt="오른쪽 화살표"
+        />
+      </div> */}
+      <div className="itemBox">
+        <img className="shoesImg" src={shoesImg} alt="{shoesName}" />
+        <div className="shoesInfo">
+          <div className="shoesName">{shoesName}</div>
+          <div className="price">{price} 원</div>
+        </div>
+        <div className="category">{category}</div>
       </div>
-
-      <div className="category">{category}</div>
+      {/* {recommendItem.map(shoes => {
+        return <RecommendItem key={shoes.id} {...shoes} />;
+      })} */}
     </div>
   );
 }
