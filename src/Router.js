3edb5f08b0340837ae0dd7cb15d0083839ca56ac
shinheylynn <heylub@gmail.com>
2023-02-09 19:23:13 +0900

    피드백 반영완료

--- src/Router.js
+++ src/Router.js
@@ -1,36 +1,28 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Container from './components/Container/Container';
 import Main from './pages/Main/Main';
 import SignUp from './pages/SignUp/SignUp';
-import Login from './pages/Login/Login/Login';
-import NewUser from './pages/Login/NewUser/NewUser';
-import Password from './pages/Login/Password/Password';
+import Login from './pages/Login/Login';
 import ProductList from './pages/ProductList/ProductList';
 import ProductDetail from './pages/ProductDetail/ProductDetail';
 import Cart from './pages/Cart/Cart';
 import Payment from './pages/Payment/Payment';
-import Nav from './components/Nav/Nav';
-import Footer from './components/Footer/Footer';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Nav />}>
+        <Route element={<Container />}>
           <Route path="/" element={<Main />} />
-          <Route path="/login/newuser" element={<NewUser />} />
-          <Route path="/login/password" element={<Password />} />
           <Route path="/product-list" element={<ProductList />} />
           <Route path="/product-detail" element={<ProductDetail />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/payment" element={<Payment />} />
         </Route>
-        <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
-      <Footer />
     </BrowserRouter>
   );
 };
-
-export default Router;
