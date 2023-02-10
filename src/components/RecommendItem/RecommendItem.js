c71e891bfb759a70d36e5d4ca8c16288a2f8a8f7
이유진 <yjoct13@gmail.com>
2023-02-10 19:01:57 +0900

    Modify: 추천 상품 컴포넌트 수정중

--- src/components/RecommendItem/RecommendItem.js
+++ src/components/RecommendItem/RecommendItem.js
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from 'react';
 import './RecommendItem.scss';
 
 function RecommendItem({ shoesImg, shoesName, category, price }) {
-  // const [recommendItem, setRecommendItem] = useState([]);
+  const onClickBtn = () => {
+    d;
+  };
 
   // useEffect(() => {
   //   fetch('/data/shoesData.json')
