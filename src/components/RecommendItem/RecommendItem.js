e113cb5b855683b3545eb92980177a83fe6e1186
이유진 <yjoct13@gmail.com>
2023-02-11 23:47:11 +0900

    recommend item 수정중

--- src/components/RecommendItem/RecommendItem.js
+++ src/components/RecommendItem/RecommendItem.js
@@ -4,6 +4,23 @@ import './RecommendItem.scss';
 function RecommendItem() {
   const [recommendItem, setRecommendItem] = useState([]);
 
+  const [count, setCount] = useState(0);
+
+  const nextBtn = () => {
+    setCount(count => (count < 8 ? count + 1 : (count = 0)));
+  };
+
+  const prevBtn = () => {
+    setCount(count => (count > 0 ? count - 1 : null));
+  };
+
+  // useEffect(() => {
+  //   const interval = setTimtout(() => {
+  //     nextBtn();
+  //   }, 3000);
+  //   return () => clearTimeout(interval);
+  // });
+
   useEffect(() => {
     fetch('/data/shoesData.json')
       .then(res => res.json())
@@ -12,24 +29,32 @@ function RecommendItem() {
 
   return (
     <div>
-      {recommendItem.map(shoes => 
-        {return key={shoes.id} 
-        <div>
-            <div className="itemBox">
-        <img
-          className="shoesImg"
-          src={shoes.shoesImg}
-          alt="{shoes.shoesName}"
-        />
-        <div className="shoesInfo">
-          <div className="shoesName">{shoes.shoesName}</div>
-          <div className="price">{shoes.price} 원</div>
-        </div>
-        <div className="category">{shoes.category}</div>
-      </div>
-      </div>}
-    
-        )}
+      {recommendItem.map(shoes => {
+        return (
+          <div
+            key={shoes.id}
+            className="itemBox"
+            style={{ transform: `translateX(${count * -1080}px)` }}
+          >
+            <button className="prev" onClick={prevBtn}>
+              〈
+            </button>
+            <button className="next" onClick={nextBtn}>
+              〉
+            </button>
+            <img
+              className="shoesImg"
+              src={shoes.shoesImg}
+              alt="{shoes.shoesName}"
+            />
+            <div className="shoesInfo">
+              <div className="shoesName">{shoes.shoesName}</div>
+              <div className="price">{shoes.price} 원</div>
+            </div>
+            <div className="category">{shoes.category}</div>
+          </div>
+        );
+      })}
     </div>
   );
 }
@@ -46,39 +71,4 @@ function RecommendItem() {
           alt="오른쪽 화살표"
         />
       </div> */
-
-//   {recommendItem.map({shoesImg, shoesName, category, price} => (
-//     <div className="itemBox">
-//     <img className="shoesImg" src={shoesImg} alt="{shoesName}" />
-//     <div className="shoesInfo">
-//       <div className="shoesName">{shoesName}</div>
-//       <div className="price">{price} 원</div>
-//     </div>
-//     <div className="category">{category}</div>
-//   </div>
-// ))}
-
 export default RecommendItem;
-
-
-
-return (
-  <div>
-    {recommendItem.map(shoes => (
-      key={shoes.id},
-      <div className="itemBox">
-        <img
-          className="shoesImg"
-          src={shoes.shoesImg}
-          alt="{shoes.shoesName}"
-        />
-        <div className="shoesInfo">
-          <div className="shoesName">{shoes.shoesName}</div>
-          <div className="price">{shoes.price} 원</div>
-        </div>
-        <div className="category">{shoes.category}</div>
-      </div>
-    ))}
-  </div>
-);
-}
