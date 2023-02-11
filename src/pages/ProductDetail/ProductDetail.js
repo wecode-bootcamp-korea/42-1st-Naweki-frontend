2eecc6407609313d4e3aa5b64ff9a647ea4b5994
이유진 <yjoct13@gmail.com>
2023-02-11 22:15:45 +0900

    Modify:상세 페이지 수정중

--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -43,43 +43,60 @@ function ProductDetail() {
         </div>
       </div>
       <div className="productInfoContainer">
-        <div className="infoTopContainer">
-          <div className="productInfo">
-            <div className="productName">나위키 에얼 포스 1 '03</div>
-            <div className="productCategory">여성 신발</div>
+        <div className="productInfo">
+          <div className="productName">나위키 에얼 포스 1 '03</div>
+          <div className="productCategory">여성 신발</div>
+          <div className="priceContainer">
             <div className="productPrice">125,100 원</div>
-            <div className="OriginalPrice">139,000 원</div>
+            <div className="originalPrice">139,000 원</div>
           </div>
-          <div className="OtherColorContainer">
-            <img
-              className="OtherColorBlack"
-              src="/images/에얼포스블랙.jpg"
-              alt="나위키 에얼 포스 1 '03 블랙"
-            />
-            <img
-              className="OtherColorPink"
-              src="https://images.unsplash.com/photo-1608667508764-33cf0726b13a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHNuZWFrZXJzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
-              alt="나위키 에얼 포스 1 '03 핑크"
-            />
-            <img
-              className="OtherColorBlue"
-              src="https://cdn.pixabay.com/photo/2020/07/15/18/32/sneakers-5408674_1280.png"
-              alt="나위키 에얼 포스 1 '03 블루"
-            />
+        </div>
+        <div className="otherColorContainer">
+          <img
+            className="otherColorBlack"
+            src="/images/에얼포스블랙.jpg"
+            alt="나위키 에얼 포스 1 '03 블랙"
+          />
+          <img
+            className="otherColorPink"
+            src="https://images.unsplash.com/photo-1608667508764-33cf0726b13a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHNuZWFrZXJzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+            alt="나위키 에얼 포스 1 '03 핑크"
+          />
+          <img
+            className="otherColorBlue"
+            src="https://cdn.pixabay.com/photo/2020/07/15/18/32/sneakers-5408674_1280.png"
+            alt="나위키 에얼 포스 1 '03 블루"
+          />
+        </div>
+        <div className="sizeInfo">
+          <div className="sizeHeader">
+            <div className="sizeChoice">사이즈 선택</div>
+            <div className="sizeGuide">사이즈 가이드</div>
           </div>
-          <div className="sizeHeader">사이즈 선택</div>
-          <div className="sizeGuide">사이즈 가이드</div>
+
           <div className="sizeContainer">
-            <div className="size">220</div>
-            <div className="size">230</div>
-            <div className="size">240</div>
-            <div className="size">250</div>
-            <div className="size">260</div>
+            <div className="smallSize">
+              <div className="size">220</div>
+              <div className="size">225</div>
+              <div className="size">230</div>
+              <div className="size">235</div>
+              <div className="size">240</div>
+            </div>
+            <div className="bigSize">
+              <div className="size">245</div>
+              <div className="size">250</div>
+              <div className="size">255</div>
+              <div className="size">260</div>
+              <div className="size">265</div>
+            </div>
           </div>
+        </div>
+        <div className="btnContainer">
           <button className="cartBtn">장바구니</button>
           <button className="wishListBtn">위시리스트</button>
         </div>
-        <div className="infoBottomContainer">
+
+        <div className="description">
           <span className="descriptionTop">
             빛이 그대로 살아 있는 나위키 에얼 포스 1 '03은 농구 아이콘의 익숙한
             디자인을 신선하게 재해석합니다. 산뜻한 가죽과 과감한 컬러, 딱 완벽할
@@ -91,10 +108,12 @@ function ProductDetail() {
             </li>
             <li className="styleDescription">스타일 : CC1928-100</li>
           </ul>
-          <span className="detailDescription">상품 상세 정보 보기</span>
-          <h3 className="freeDelivery">무료 배송 및 반품</h3>
-          <h3 className="review">리뷰 (3712) </h3>
-          <h3 className="moreInfo">추가 정보</h3>
+          <div className="detailDescription">상품 상세 정보 보기</div>
+          <div className="detailService">
+            <button className="freeDelivery">무료 배송 및 반품</button>
+            <button className="review">리뷰 (3712) </button>
+            <button className="moreInfo">추가 정보</button>
+          </div>
         </div>
       </div>
     </div>
