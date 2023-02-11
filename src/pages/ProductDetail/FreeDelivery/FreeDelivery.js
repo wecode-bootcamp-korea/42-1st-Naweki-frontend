1f99f7427afd3b387d99bb157bc897c3fda99c75
이유진 <yjoct13@gmail.com>
2023-02-12 03:57:20 +0900

    상세 페이지 수정중

--- src/pages/ProductDetail/FreeDelivery/FreeDelivery.js
+++ src/pages/ProductDetail/FreeDelivery/FreeDelivery.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './FreeDelivery.scss';
 
 function FreeDelivery() {
   return (
@@ -12,19 +13,31 @@ function FreeDelivery() {
           가능합니다.
         </p>
         <p>일반 배송</p>
-        <button>자세히 알아보기</button>
         <p>반품</p>
-        <button>자세히 알아보기</button>
+        <p className="deliveryDetail">자세히 알아보기</p>
       </div>
-      <div className="todayService">
-        오늘도착 서비스 • 이용시간: 낮 12시까지 결제 시, 당일 도착 (일요일,
-        공휴일 제외) • 서비스지역: 서울∙과천∙의왕∙군포∙수원∙성남∙안양시 전체,
-        용인시 수지구∙기흥구, 부천시 중동∙상동∙심곡동 • 서비스비용: 5,000원
-        자세히 알아보기
+
+      <div className="todayDeliveryInfo">
+        <p className="todayDelivery">오늘도착 서비스</p>
+        <p className="time">
+          • 이용시간: 낮 12시까지 결제 시, 당일 도착 (일요일, 공휴일 제외)
+        </p>
+        <p className="region">
+          • 서비스 지역: 서울∙과천∙의왕∙군포∙수원∙성남∙안양시 전체, 용인시
+          수지구∙기흥구, 부천시 중동∙상동∙심곡동
+        </p>
+        <p>• 서비스비용: 5,000원</p>
+        <p className="todayDeliveryDetail">자세히 알아보기</p>
       </div>
-      <div className="afterService">
-        A/S 안내 • 나이키 온라인에서 구매하신 제품에 대한 A/S 는 나이키코리아
-        고객센터(080-022-0182)에서 유선으로만 접수 가능합니다. 자세히 알아보기
+
+      <div className="afterServiceInfo">
+        <p className="afterService">A/S 안내</p>
+        <p className="afterServiceContent">
+          • 나이키 온라인에서 구매하신 제품에 대한 A/S 는 나이키코리아 고객센터(
+          <div className="serviceNumber">080-123-4567</div>)에서 유선으로만 접수
+          가능합니다.
+        </p>
+        <p className="serviceDetail"> 자세히 알아보기</p>
       </div>
     </div>
   );
--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -1,9 +1,17 @@
 import React, { useState } from 'react';
+import EmptyHeartBtn from './EmptyHeartBtn/EmptyHeartBtn';
 import FreeDelivery from './FreeDelivery/FreeDelivery';
+import FullHeartBtn from './FullHeartBtn/FullHeartBtn';
 import './ProductDetail.scss';
 
 function ProductDetail() {
   const [visible, setVisible] = useState(false);
+
+  const [heart, setHeart] = useState(false);
+
+  const onClickBtn = () => {
+    setHeart(true);
+  };
 
   return (
     <div className="productDetail">
@@ -46,59 +54,74 @@ function ProductDetail() {
         </div>
       </div>
       <div className="productInfoContainer">
-        <div className="productInfo">
-          <div className="productName">나위키 에얼 포스 1 '03</div>
-          <div className="productCategory">여성 신발</div>
-          <div className="priceContainer">
-            <div className="productPrice">125,100 원</div>
-            <div className="originalPrice">139,000 원</div>
+        <div className="productInfoContainer">
+          <div className="productInfo">
+            <div className="productName">나위키 에얼 포스 1 '03</div>
+            <div className="productCategory">여성 신발</div>
+            <div className="priceContainer">
+              <div className="productPrice">125,100 원</div>
+              <div className="originalPrice">139,000 원</div>
+            </div>
           </div>
-        </div>
-        <div className="otherColorContainer">
-          <img
-            className="otherColorBlack"
-            src="/images/에얼포스블랙.jpg"
-            alt="나위키 에얼 포스 1 '03 블랙"
-          />
-          <img
-            className="otherColorPink"
-            src="https://images.unsplash.com/photo-1608667508764-33cf0726b13a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHNuZWFrZXJzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
-            alt="나위키 에얼 포스 1 '03 핑크"
-          />
-          <img
-            className="otherColorBlue"
-            src="https://cdn.pixabay.com/photo/2020/07/15/18/32/sneakers-5408674_1280.png"
-            alt="나위키 에얼 포스 1 '03 블루"
-          />
-        </div>
-        <div className="sizeInfo">
-          <div className="sizeHeader">
-            <div className="sizeChoice">사이즈 선택</div>
-            <div className="sizeGuide">사이즈 가이드</div>
+          <div className="otherColorContainer">
+            <img
+              className="otherColorBlack"
+              src="/images/에얼포스블랙.jpg"
+              alt="나위키 에얼 포스 1 '03 블랙"
+            />
+            <img
+              className="otherColorPink"
+              src="https://images.unsplash.com/photo-1608667508764-33cf0726b13a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHNuZWFrZXJzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+              alt="나위키 에얼 포스 1 '03 핑크"
+            />
+            <img
+              className="otherColorBlue"
+              src="https://cdn.pixabay.com/photo/2020/07/15/18/32/sneakers-5408674_1280.png"
+              alt="나위키 에얼 포스 1 '03 블루"
+            />
+          </div>
+          <div className="sizeInfo">
+            <div className="sizeHeader">
+              <div className="sizeChoice">사이즈 선택</div>
+              <div className="sizeGuide">사이즈 가이드</div>
+            </div>
+
+            <div className="sizeContainer">
+              <div className="smallSize">
+                <div className="size">220</div>
+                <div className="size">225</div>
+                <div className="size">230</div>
+                <div className="size">235</div>
+                <div className="size">240</div>
+              </div>
+              <div className="bigSize">
+                <div className="size">245</div>
+                <div className="size">250</div>
+                <div className="size">255</div>
+                <div className="size">260</div>
+                <div className="size">265</div>
+              </div>
+            </div>
+          </div>
+          <div className="btnContainer" onClick={onClickBtn}>
+            {heart ? <FullHeartBtn /> : <EmptyHeartBtn />}
           </div>
 
-          <div className="sizeContainer">
-            <div className="smallSize">
-              <div className="size">220</div>
-              <div className="size">225</div>
-              <div className="size">230</div>
-              <div className="size">235</div>
-              <div className="size">240</div>
-            </div>
-            <div className="bigSize">
-              <div className="size">245</div>
-              <div className="size">250</div>
-              <div className="size">255</div>
-              <div className="size">260</div>
-              <div className="size">265</div>
-            </div>
-          </div>
-        </div>
-        <div className="btnContainer">
+          {/* <div className="btnContainer">
           <button className="cartBtn">장바구니</button>
-          <button className="wishListBtn">위시리스트</button>
-        </div>
+          <button className="wishListBtn" onClick={onClickBtn}>
+            위시리스트{' '}
+            <button className="emptyHeartBtn">
+              <img
+                className="emptyHeartImg"
+                src="https://cdn-icons-png.flaticon.com/128/1077/1077035.png"
+                alt="빈 하트"
+              />
+            </button>
+          </button>
 
+          {heart ? 꽉찬하트 : 빈하트} */}
+        </div>
         <div className="description">
           <span className="descriptionTop">
             빛이 그대로 살아 있는 나위키 에얼 포스 1 '03은 농구 아이콘의 익숙한
@@ -119,7 +142,9 @@ function ProductDetail() {
                 setVisible(!visible);
               }}
             >
-              {visible ? '무료 배송 및 반품' : '안눌렀을때'}
+              {visible
+                ? '무료 배송 및 반품 위쪽 화살표'
+                : '무료 배송 및 반품 아래 화살표'}
               {visible && <FreeDelivery />}
             </button>
             <button className="review">리뷰 (3712) </button>
