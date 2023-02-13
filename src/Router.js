8fa81f8f37e6ff786c4de4f620f5daa9bba84368
shinheylynn <heylub@gmail.com>
2023-02-13 18:32:35 +0900

    공통코드 통일화 작업

--- src/Router.js
+++ src/Router.js
@@ -3,11 +3,11 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Container from './components/Container/Container';
 import Main from './pages/Main/Main';
 import SignUp from './pages/SignUp/SignUp';
-import Login from './pages/Login/Login/Login';
 import ProductList from './pages/ProductList/ProductList';
 import ProductDetail from './pages/ProductDetail/ProductDetail';
 import Cart from './pages/Cart/Cart';
 import Payment from './pages/Payment/Payment';
+import Login from './pages/Login/Login/Login';
 import NewUser from './pages/Login/NewUser/NewUser';
 import Password from './pages/Login/Password/Password';
 
@@ -17,12 +17,13 @@ const Router = () => {
       <Routes>
         <Route element={<Container />}>
           <Route path="/" element={<Main />} />
+          <Route path="/signup" element={<SignUp />} />
           <Route path="/product-list" element={<ProductList />} />
           <Route path="/product-detail" element={<ProductDetail />} />
           <Route path="/cart" element={<Cart />} />
-          <Route path="/payment" element={<Payment />} />
         </Route>
-        <Route path="/signup" element={<SignUp />} />
+        <Route path="/payment" element={<Payment />} />
+        {/* TODO: 추후에 추가예정; Nav 변경, Footer는 포함 */}
         <Route path="/login" element={<Login />} />
         <Route path="/newuser" element={<NewUser />} />
         <Route path="/password" element={<Password />} />
