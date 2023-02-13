49f1c8bc9c53558d5ad6c5c0baa549202a3160ef
macbook <macbook@jungee.local>
2023-02-13 17:47:24 +0900

    Modify: productData 파일 수정

--- src/pages/ProductList/Product/Product.js
+++ src/pages/ProductList/Product/Product.js
@@ -2,16 +2,17 @@ import React from 'react';
 import './Product.scss';
 
 const Product = ({ ...product }) => {
-  const { alt, img, isNew, name, style, color, price, reduced_price } = product;
+  const { img, created_at, name, style, color_id, price, reduced_price } =
+    product;
 
   return (
     <div className="product">
-      <img className="productImg" alt={alt} src={img} />
+      <img className="productImg" alt="Product Img" src={img} />
       <div className="productDesc">
-        {isNew === true && <span className="new">신상품</span>}
+        {created_at === true && <span className="new">신상품</span>}
         <span className="name">{name}</span>
         <span className="style">{style}</span>
-        <span className="color">{color}개 색상</span>
+        <span className="color">{color_id}개 색상</span>
         {product.reduced_price !== '' ? (
           <div className="reducedPriceWrapper">
             <span className="reducedPrice">{reduced_price} 원</span>
