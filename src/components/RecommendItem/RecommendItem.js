b203bd9aff184ffac9252e35d94608f1411b14fb
yujinni <yjoct13@gmail.com>
2023-02-13 20:31:22 +0900

    Add : 추천 상품 슬라이드 컴포넌트 레이아웃 및 기능 구현

--- src/components/RecommendItem/RecommendItem.js
+++ src/components/RecommendItem/RecommendItem.js
@@ -34,16 +34,20 @@ function RecommendItem() {
       <div className="titleWrapper">
         <div className="title">Recommend Items</div>
         <div className="buttonWrapper">
-          <button
-            className="leftButton"
+          <img
+            className="leftBtn"
+            src="../../images/왼쪽화살표.jpg"
+            alt="왼쪽 화살표"
             onClick={onClickLeftBtn}
             disabled={!checkLeftBtnActive()}
-          >
-            {'<'}
-          </button>
-          <button onClick={onClickRightBtn} disabled={!checkRightBtnActive()}>
-            {'>'}
-          </button>
+          />
+          <img
+            className="rightBtn"
+            src="../../images/오른쪽화살표.png"
+            alt="오른쪽 화살표"
+            onClick={onClickRightBtn}
+            disabled={!checkRightBtnActive()}
+          />
         </div>
       </div>
       <div className="imageWrapper">
@@ -51,12 +55,6 @@ function RecommendItem() {
           className="image-mover"
           style={{ transform: `translateX(-${showStartIdx * (100 / 9)}%)` }}
         >
-          {/* {recommendItem.map(imgUrl => (
-            <div key={imgUrl.id} className="image">
-              <img src={imgUrl.shoesImg} alt="추천 상품" />
-            </div>
-          ))} */}
-
           {recommendItem.map(imgUrl => {
             return (
               <div key={imgUrl.id} className="image">
@@ -80,78 +78,3 @@ function RecommendItem() {
 }
 
 export default RecommendItem;
-// const [recommendItem, setRecommendItem] = useState([]);
-
-// const [showStartIdx, setShowStartIdx] = useState(0);
-
-// const numOfShownCard = 3;
-// const checkLeftBtnActive = () => showStartIdx > 0;
-// const checkRightBtnActive = () =>
-//   showStartIdx < recommendItem.length - numOfShownCard;
-
-// const onClickLeftBtn = () => {
-//   if (checkLeftBtnActive()) {
-//     setShowStartIdx(showStartIdx - 1);
-//   }
-// };
-
-// const onClickRightBtn = () => {
-//   if (checkRightBtnActive()) {
-//     setShowStartIdx(showStartIdx + 1);
-//   }
-// };
-
-// const showImgs = recommendItem.slice(
-//   showStartIdx,
-//   showStartIdx + numOfShownCard
-// );
-
-// useEffect(() => {
-//   fetch('/data/shoesData.json')
-//     .then(res => res.json())
-//     .then(data => setRecommendItem(data));
-// }, []);
-
-// return (
-//   <div className="container">
-//     <div className="titleWrapper">
-//       <div className="title">You Might Also Like</div>
-//       <div className="buttonWrapper">
-//         <button onClick={onClickLeftBtn} disabled={!checkLeftBtnActive()}>
-//           {'<'}
-//         </button>
-//         <button onClick={onClickRightBtn} disabled={!checkRightBtnActive()}>
-//           {'>'}
-//         </button>
-//       </div>
-//     </div>
-//     <div className="image-wrapper">
-//       {showImgs.map(img => (
-//         <div className="image">
-//           <img src={img} />
-//         </div>
-//       ))}
-//     </div>
-//   </div>
-// <div>
-// {recommendItem.map(shoes => {
-//   return (
-//     <div key={shoes.id} className="itemBox">
-//       <img
-//         className="shoesImg"
-//         src={shoes.shoesImg}
-//         alt="{shoes.shoesName}"
-//       />
-//       <div className="shoesInfo">
-//         <div className="shoesName">{shoes.shoesName}</div>
-//         <div className="price">{shoes.price} 원</div>
-//       </div>
-//       <div className="category">{shoes.category}</div>
-//     </div>
-//   );
-// })}
-// </div>
-//   );
-// }
-
-// export default RecommendItem;
