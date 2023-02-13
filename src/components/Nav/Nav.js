5045580540ed76ac7989bba8f7250531976bb34c
macbook <macbook@jungee.local>
2023-02-13 18:18:16 +0900

    Modify: 수정 요청 반영

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,32 +1,8 @@
 import React from 'react';
-// import { menuData } from './data/menuData';
 import './Nav.scss';
 
 const Nav = () => {
-  return (
-    <nav className="nav">
-      <div className="logoWrapper">
-        <img className="logo" alt="Logo Img" src="images/logo.jpg" />
-      </div>
-      <div className="menuWrapper">
-        <ul className="menuLists">
-          {/* 상수데이터 처리하기*/}
-          <li className="menuList">New Releases</li>
-          <li className="menuList">Men</li>
-          <li className="menuList">Women</li>
-          <li className="menuList">Kids</li>
-          <li className="menuList">Sale</li>
-          <li className="menuList">SNEAKERS</li>
-          <li className="menuList">FEEL YOUR ALL</li>
-        </ul>
-      </div>
-      <div className="functionWrapper">
-        <input className="searchBar" type="text" placeholder="검색" />
-        <i className="far fa-heart" />
-        <i className="fas fa-shopping-bag" />
-      </div>
-    </nav>
-  );
+  return <p>안녕하세요</p>;
 };
 
 export default Nav;
