6aebb60c48cc717c600221b9300056852e1093d3
shinheylynn <heylub@gmail.com>
2023-02-14 13:55:52 +0900

    수정중

--- src/pages/Cart/CartList/CartList.js
+++ src/pages/Cart/CartList/CartList.js
@@ -1,10 +1,19 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './CartList.scss';
 
 export const CartList = ({ list }) => {
   const [heart, setHeart] = useState(true);
   const handleLike = e => {
     setHeart(previousImg => !previousImg);
+  };
+
+  const [items, setItems] = useState([]);
+
+  const deleteItem = () => {
+    fetch('URI', {
+      method: 'DELETE',
+      headers: { Authorization: localStorage.getItem('') },
+    }).then(res => {});
   };
 
   return (
