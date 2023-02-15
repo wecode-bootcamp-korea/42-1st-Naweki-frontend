beaff0b1b0a41fcd33d881ef338507fc6db3cdeb
macbook <macbook@jungee.local>
2023-02-15 10:43:31 +0900

    modify

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useSearchParams } from 'react-router-dom';
 import Search from './Search/Search';
 import Menu from './Menu/Menu';
 import Function from './Function/Function';
@@ -7,9 +7,41 @@ import './Nav.scss';
 
 const Nav = () => {
   const [clicked, setClicked] = useState(false);
+  // const [searchParams, setSearchParams] = useSearchParams();
+  // const category = searchParams.get('category');
+  // const limit = searchParams.get('limit');
+  // const page = searchParams.get('page');
+  // const sort = searchParams.get('sort');
+  // const subCategory = searchParams.get('sub_category');
+  // const gender = searchParams.get('gender');
+
+  const [products, setProducts] = useState([]);
+
   const searchClicked = () => {
     setClicked(prev => !prev);
   };
+
+  // useEffect(() => {
+  //    fetch( 'http://10.58.52.93:3000/products?category=${category}&limit=${limit}&page=${page}&sort=${sort}&sub_category=${subCategory}&gender=${gender}', {
+  //   method: ‘GET’,
+  //   })
+  //   .then(res=>res.json())
+  //   .then(data=> {
+  //   setProducts(data);
+  //   });
+  //   },  [category, limit, page, sort, subCategory, gender]);
+
+  // localhost:3000/products?category=신발&limit=5&page=1&sort=productIdDesc&sub_category=라이프스타일&gender=남자
+
+  useEffect(() => {
+    fetch('http://localhost:3000/products', {
+      method: 'GET',
+    })
+      .then(res => res.json())
+      .then(data => {
+        setProducts(data);
+      });
+  }, []);
 
   return (
     <nav className="nav">
