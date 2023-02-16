79e081e191fb8f3fe49c49ab3eab06c364efc7c8
shinheylynn <heylub@gmail.com>
2023-02-16 13:17:59 +0900

    add: config.js 파일 수정

--- src/config.js
+++ src/config.js
@@ -1,2 +1,6 @@
 const BASE_URL = 'http://';
-export const GET_PRODUCT_API = `${BASE_URL}/products`;
+export const GET_LOOKUP_API = `${BASE_URL}/users/lookup`;
+export const GET_LOGIN_API = `${BASE_URL}/users/login`;
+export const GET_SIGNUP_API = `${BASE_URL}/users/signup`;
+
+export const GET_PRODUCTS_API = `${BASE_URL}/products`;
