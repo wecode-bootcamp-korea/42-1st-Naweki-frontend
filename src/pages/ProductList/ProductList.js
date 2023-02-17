7e5058be74b46e5ec754002ccbb61c96669b3c6f
macbook <macbook@jungee.local>
2023-02-17 09:41:40 +0900

    Modify: 기능 추가

--- src/pages/ProductList/ProductList.js
+++ src/pages/ProductList/ProductList.js
@@ -4,6 +4,7 @@ import Aside from './Aside/Aside';
 import Banner from './Banner/Banner';
 import Product from './Product/Product';
 import { CATEGORY_DATA } from './Aside/data/categoryData';
+import React from 'react';
 import './ProductList.scss';
 
 const ProductList = () => {
@@ -14,48 +15,21 @@ const ProductList = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const limit = searchParams.get('limit');
   const page = searchParams.get('page');
-  const [posts, setPosts] = useState(0);
-
-  // TODO: mock data 사용 시
-  // useEffect(() => {
-  //   fetch('./data/productData.json', {
-  //     method: 'GET',
-  //   })
-  //     .then(res => res.json())
-  //     .then(data => {
-  //       setProducts(data);
-  //     });
-  // }, []);
-
-  // TODO: api 연결 시
-  // useEffect(() => {
-  //   fetch(
-  //     `http://10.58.52.114:3000/products${category}?page=${page}&limit=${limit}`,
-  //     {
-  //       method: 'GET',
-  //     }
-  //   )
-  //     .then(res => {
-  //       return res.json();
-  //     })
-  //     .then(data => {
-  //       setProducts(data.data);
-  //       setPosts(data);
-  //     });
-  // }, [page, limit, category]);
 
   useEffect(() => {
-    fetch('http://10.58.52.114:3000/products', {
-      method: 'GET',
-    })
+    fetch(
+      `http://10.58.52.114:3000/products${category}?page=${page}&limit=${limit}`,
+      {
+        method: 'GET',
+      }
+    )
       .then(res => {
         return res.json();
       })
       .then(data => {
         setProducts(data.data);
-        setPosts(data);
       });
-  }, []);
+  }, [page, limit, category]);
 
   const onClickFilter = () => {
     setIsFilterClicked(prev => !prev);
